d024ea351e6b24debfaa02a395d3620cfedb69d5
jaychaturvedi <jaychaturvedi18@gmail.com>
2020-04-07 23:26:08 +0530

    fifth commit

--- app/db/dev/pool.js
+++ app/db/dev/pool.js
@@ -5,10 +5,13 @@ dotenv.config();
 
 const pool = new Pool({
     user:"postgres",
-    password:"qwerty",
+    password:"J@y@1996",
     host:"localhost",
     port:5432,
     database:"transport"
 });
+
+// const databaseConfig = { connectionString: process.env.DATABASE_URL };
+// const pool = new Pool(databaseConfig);
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,4 +1,47 @@
-//commentatory
-//commentatory2
-//commentatory3
-//commentatory4
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const bodyparser = require('body-parser');
+const cors = require('cors');
+//const jsonparser = ors = require('cos');
+// app.use(bodyparser.json());
+const jsonparser = bodyparser.json();
+var courses = [
+  { id: 1, name: "kaka" },
+  { id: 2, name: "chichi" },
+  { id: 3, name: "hihi" },
+];
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/accounts/", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/accounts/:id/", jsonparser, (req, res) => {
+  const course = courses.find( c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("course not found");
+    return;
+  }
+  res.send(course);
+});
+
+app.post("/posts", (req, res) => {
+    console.log(JSON.stringify( req.body.name));
+    const course = {
+    id: courses.length + 1,
+    name: JSON.stringify( req.body.name)
+  };
+  courses.push(course);
+  res.send(courses);
+});
+
+dotenv.config();
+// app.use(cors);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server is running on port " + port);
+});
